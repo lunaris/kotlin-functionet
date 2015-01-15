0f259592d15005e628650f912949fd74ec83ce3e
Will Jones <will@sacharissa.co.uk>
2015-01-15 17:03:53 +0000

    Set up a skeletal mod using Kotlin and Gradle.

---
+++ src/main/kotlin/uk/co/sacharissa/functionet/Functionet.kt
@@ -0,0 +1,16 @@
+package uk.co.sacharissa.functionet
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.FMLPreInitializationEvent
+import cpw.mods.fml.common.registry.GameRegistry
+
+val modId = "functionet";
+val modName = "Functionet"
+val modVersion = "1.0";
+
+Mod(modid = modId, name = modName, version = modVersion) class Functionet {
+  EventHandler fun preInit(e: FMLPreInitializationEvent) {
+  }
+}
+
